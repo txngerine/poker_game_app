1a2485239f8a125488c5d8f4577a83a514a77682
nyouibou <akshaits4@gmail.com>
2025-03-31 12:21:25 +0530

    FEAT SORTING

--- lib/widget/affiliate_players_button_widget.dart
+++ lib/widget/affiliate_players_button_widget.dart
@@ -1,3 +1,247 @@
+// import 'package:flutter/material.dart';
+// import 'package:pokerpad/widget/affiliate_bonuse_button_widget.dart';
+// import 'package:pokerpad/widget/affiliate_players_listview.dart';
+// import 'package:pokerpad/widget/build_sub_heading_text.dart';
+// import 'package:provider/provider.dart';
+
+// import '../provider/affiliated_button_provider.dart';
+
+// class AffiliatePlayersButtonWidget extends StatefulWidget {
+//   const AffiliatePlayersButtonWidget({super.key});
+
+//   @override
+//   State<AffiliatePlayersButtonWidget> createState() =>
+//       _AffiliatePlayersButtonWidgetState();
+// }
+
+// class _AffiliatePlayersButtonWidgetState
+//     extends State<AffiliatePlayersButtonWidget> {
+//       final TextEditingController _searchController = TextEditingController();
+//   String searchQuery = "";
+//   @override
+//   Widget build(BuildContext context) {
+//     final width = MediaQuery.sizeOf(context).width;
+//     final height = MediaQuery.sizeOf(context).height;
+//     final provider = Provider.of<AffiliatedButtonProvider>(context);
+
+//     return Padding(
+//       padding: const EdgeInsets.only(top: 140),
+//       child: Container(
+//         width: width,
+//         height: height,
+//         decoration: const BoxDecoration(
+//             image: DecorationImage(
+//                 image: AssetImage("assets/images/affiliate screen/aff_bg.png"),
+//                 fit: BoxFit.cover)),
+//         child: Padding(
+//           padding: const EdgeInsets.all(8.0),
+//           child: Column(
+//             children: [
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: [
+//                   ClipRRect(
+//                     child: Container(
+//                         width: width / 2.1,
+//                         height: 50,
+//                         decoration: const BoxDecoration(
+//                           image: DecorationImage(
+//                             image: AssetImage(
+//                                 "assets/images/affiliate screen/aff_textfield1.png"),
+//                             fit: BoxFit.cover,
+//                           ),
+//                         ),
+//                         child: const Row(
+//                           mainAxisAlignment: MainAxisAlignment.spaceAround,
+//                           children: [
+//                             BuildSubHeadingText(
+//                               text: "NO.OF PLAYERS",
+//                               color: Colors.white,
+//                               fontSize: 10,
+//                             ),
+//                             BuildSubHeadingText(
+//                               text: "365",
+//                               color: Colors.white,
+//                               fontSize: 13,
+//                             ),
+//                           ],
+//                         )),
+//                   ),
+//                   ClipRRect(
+//                     child: Container(
+//                         width: width / 2.1,
+//                         height: 50,
+//                         decoration: const BoxDecoration(
+//                           image: DecorationImage(
+//                             image: AssetImage(
+//                                 "assets/images/affiliate screen/aff_texfield2.png"),
+//                             fit: BoxFit.cover,
+//                           ),
+//                         ),
+//                         child: const Row(
+//                           mainAxisAlignment: MainAxisAlignment.spaceAround,
+//                           children: [
+//                             BuildSubHeadingText(
+//                               text: "TOTAL RAKE",
+//                               color: Colors.white,
+//                               fontSize: 10,
+//                             ),
+//                             BuildSubHeadingText(
+//                               text: "\$50,214",
+//                               color: Colors.white,
+//                               fontSize: 13,
+//                             ),
+//                           ],
+//                         )),
+//                   ),
+//                 ],
+//               ),
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: [
+//                   GestureDetector(
+//                     onTap: () {
+//                       provider.setClicked(false);
+//                       Navigator.pop(context);
+//                       provider.toggleBonusClicked(false);
+//                     },
+//                     child: Image.asset(
+//                         width: width / 4.2,
+//                         "assets/images/affiliate screen/players active.png"),
+//                   ),
+//                   GestureDetector(
+//                     onTap: () {
+//                       print("clicked bonus button");
+//                       provider.toggleBonusClicked(true);
+//                       showDialog(
+//                         barrierDismissible: false,
+//                         context: context,
+//                         builder: (context) {
+//                           return const AffiliateBonusButtonWidget();
+//                         },
+//                       );
+//                     },
+//                     child: Image.asset(
+//                         width: width / 4.2,
+//                         "assets/images/affiliate screen/bonuses passive.png"),
+//                   ),
+//                   ClipRRect(
+//                     child: Container(
+//                         width: width / 2.1,
+//                         height: 50,
+//                         decoration: const BoxDecoration(
+//                           image: DecorationImage(
+//                             image: AssetImage(
+//                                 "assets/images/affiliate screen/aff_texfield2.png"),
+//                             fit: BoxFit.cover,
+//                           ),
+//                         ),
+//                         child: const Row(
+//                           mainAxisAlignment: MainAxisAlignment.spaceAround,
+//                           children: [
+//                             BuildSubHeadingText(
+//                               text: "YOUR COMMISSION",
+//                               color: Colors.white,
+//                               fontSize: 10,
+//                             ),
+//                             BuildSubHeadingText(
+//                               text: "\$36,214",
+//                               color: Colors.white,
+//                               fontSize: 13,
+//                             ),
+//                           ],
+//                         )),
+//                   ),
+//                 ],
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.all(3.0),
+//                 child: ClipRRect(
+//                   child: Container(
+//                     width: width,
+//                     height: height / 13,
+//                     decoration: const BoxDecoration(
+//                         image: DecorationImage(
+//                             image: AssetImage(
+//                                 "assets/images/affiliate screen/search frame.png"))),
+//                     child: Row(
+//                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                       children: [
+//                         const SizedBox(
+//                           width: 3,
+//                         ),
+//                         SizedBox(
+//                           width: 80,
+//                           height: 50,
+//                           child: Material(
+//                             color: Colors.transparent,
+//                             child: TextField(
+//                               controller: _searchController,
+//                               decoration: const InputDecoration(
+//                                 hintText: "   SEARCH",
+//                                 hintStyle: TextStyle(
+//                                   color: Colors.white,
+//                                   fontSize: 11,
+//                                 ),
+//                                 contentPadding:
+//                                     EdgeInsets.symmetric(horizontal: 10),
+//                                 border: InputBorder.none,
+//                               ),
+//                               style: const TextStyle(color: Colors.white70),
+//                               onChanged: (value) {
+//                                 setState(() {
+//                                   searchQuery = value;
+//                                 });
+//                               },
+//                             ),
+//                           ),
+//                         ),
+//                         const SizedBox(
+//                           width: 20,
+//                         ),
+//                         const BuildSubHeadingText(
+//                           text: "TRANSFER",
+//                           color: Colors.white,
+//                           fontSize: 10,
+//                         ),
+//                         const BuildSubHeadingText(
+//                           text: "BALANCE",
+//                           color: Colors.white,
+//                           fontSize: 10,
+//                         ),
+//                         const BuildSubHeadingText(
+//                           text: "WINNINGS",
+//                           color: Colors.white,
+//                           fontSize: 10,
+//                         ),
+//                         const BuildSubHeadingText(
+//                           text: "RAKE",
+//                           color: Colors.white,
+//                           fontSize: 10,
+//                         ),
+//                         const BuildSubHeadingText(
+//                           text: "COMMISSION",
+//                           color: Colors.white,
+//                           fontSize: 10,
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//                AffiliatePlayersListview(
+//                   searchQuery: searchQuery,
+//                 ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
 import 'package:flutter/material.dart';
 import 'package:pokerpad/widget/affiliate_bonuse_button_widget.dart';
 import 'package:pokerpad/widget/affiliate_players_listview.dart';
@@ -16,8 +260,11 @@ class AffiliatePlayersButtonWidget extends StatefulWidget {
 
 class _AffiliatePlayersButtonWidgetState
     extends State<AffiliatePlayersButtonWidget> {
-      final TextEditingController _searchController = TextEditingController();
+  final TextEditingController _searchController = TextEditingController();
   String searchQuery = "";
+  String sortedColumn = "";
+  bool isAscending = true;
+
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.sizeOf(context).width;
@@ -199,42 +446,58 @@ class _AffiliatePlayersButtonWidgetState
                         const SizedBox(
                           width: 20,
                         ),
-                        const BuildSubHeadingText(
-                          text: "TRANSFER",
-                          color: Colors.white,
-                          fontSize: 10,
-                        ),
-                        const BuildSubHeadingText(
-                          text: "BALANCE",
-                          color: Colors.white,
-                          fontSize: 10,
-                        ),
-                        const BuildSubHeadingText(
-                          text: "WINNINGS",
-                          color: Colors.white,
-                          fontSize: 10,
-                        ),
-                        const BuildSubHeadingText(
-                          text: "RAKE",
-                          color: Colors.white,
-                          fontSize: 10,
-                        ),
-                        const BuildSubHeadingText(
-                          text: "COMMISSION",
-                          color: Colors.white,
-                          fontSize: 10,
-                        ),
+                        _buildSortableColumn("TRANSFER"),
+                        _buildSortableColumn("BALANCE"),
+                        _buildSortableColumn("WINNINGS"),
+                        _buildSortableColumn("RAKE"),
+                        _buildSortableColumn("COMMISSION"),
                       ],
                     ),
                   ),
                 ),
               ),
-               AffiliatePlayersListview(
+              Expanded(
+                child: AffiliatePlayersListview(
                   searchQuery: searchQuery,
+                  sortColumn: sortedColumn,
+                  isAscending: isAscending,
                 ),
+              ),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  // Build a sortable column header
+  Widget _buildSortableColumn(String columnName) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          if (sortedColumn == columnName) {
+            isAscending = !isAscending;
+          } else {
+            sortedColumn = columnName;
+            isAscending = true;
+          }
+        });
+      },
+      child: Row(
+        children: [
+          BuildSubHeadingText(
+            text: columnName,
+            color: Colors.white,
+            fontSize: 10,
+          ),
+          Icon(
+            sortedColumn == columnName
+                ? (isAscending ? Icons.arrow_upward : Icons.arrow_downward)
+                : Icons.swap_vert,
+            size: 12,
+            color: Colors.white,
+          ),
+        ],
       ),
     );
   }
--- lib/widget/affiliate_players_listview.dart
+++ lib/widget/affiliate_players_listview.dart
@@ -135,12 +135,39 @@ class _AffiliatePlayersListviewState extends State<AffiliatePlayersListview> {
     (index) => {
       "id": index + 100,
       "username": "CHARLIE${index + 1}",
-      "transfer": "\$432,732",
-      "balance": "\$612,432",
-      "winnings": "\$50,000",
-      "rake": "\$500",
+      "transfer": 432732 + (index * 1000), // Numeric value for sorting
+      "balance": 612432 - (index * 1000),
+      "winnings": 50000 + (index * 500),
+      "rake": 500 - (index * 10),
+      "commission": 200 + (index * 5),
     },
   );
+
+  String? sortedColumn;
+  bool isAscending = true;
+
+  void sortList(String columnName) {
+    setState(() {
+      if (sortedColumn == columnName) {
+        isAscending = !isAscending;
+      } else {
+        sortedColumn = columnName;
+        isAscending = true;
+      }
+
+      players.sort((a, b) {
+        final aValue = a[columnName];
+        final bValue = b[columnName];
+
+        if (aValue is num && bValue is num) {
+          return isAscending
+              ? aValue.compareTo(bValue)
+              : bValue.compareTo(aValue);
+        }
+        return 0;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -162,101 +189,156 @@ class _AffiliatePlayersListviewState extends State<AffiliatePlayersListview> {
         ),
         height: height / 1.560,
         width: width,
-        child: filteredPlayers.isEmpty
-            ? const Center(
-                child: BuildSubHeadingText(
-                  text: "No players found",
-                  color: Colors.white70,
-                  fontSize: 12,
-                ),
-              )
-            : ListView.builder(
-                itemCount: filteredPlayers.length,
-                itemBuilder: (context, index) {
-                  final player = filteredPlayers[index];
+        child: Column(
+          children: [
+            // Sortable Header Row
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  sortButton("transfer", "TRANSFER"),
+                  sortButton("balance", "BALANCE"),
+                  sortButton("winnings", "WINNINGS"),
+                  sortButton("rake", "RAKE"),
+                  sortButton("commission", "COMMISSION"),
+                ],
+              ),
+            ),
+            // Players List
+            Expanded(
+              child: filteredPlayers.isEmpty
+                  ? const Center(
+                      child: BuildSubHeadingText(
+                        text: "No players found",
+                        color: Colors.white70,
+                        fontSize: 12,
+                      ),
+                    )
+                  : ListView.builder(
+                      itemCount: filteredPlayers.length,
+                      itemBuilder: (context, index) {
+                        final player = filteredPlayers[index];
 
-                  return Column(
-                    children: [
-                      ClipRRect(
-                        child: Padding(
-                          padding: const EdgeInsets.only(bottom: 10),
-                          child: Container(
-                            width: width,
-                            height: height / 16,
-                            decoration: const BoxDecoration(
-                              image: DecorationImage(
-                                image: AssetImage(
-                                    "assets/images/affiliate screen/player frame.png"),
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Row(
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.all(1.0),
-                                      child: Image.asset(
-                                        "assets/images/affiliate screen/losing player (1).png",
-                                        width: width / 10,
-                                      ),
+                        return Column(
+                          children: [
+                            ClipRRect(
+                              child: Padding(
+                                padding: const EdgeInsets.only(bottom: 10),
+                                child: Container(
+                                  width: width,
+                                  height: height / 16,
+                                  decoration: const BoxDecoration(
+                                    image: DecorationImage(
+                                      image: AssetImage(
+                                          "assets/images/affiliate screen/player frame.png"),
+                                      fit: BoxFit.cover,
                                     ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(top: 13),
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Row(
                                         children: [
-                                          BuildSubHeadingText(
-                                            text: "ID:#${player['id']}",
-                                            color: Colors.white,
-                                            fontSize: 10,
+                                          Padding(
+                                            padding: const EdgeInsets.all(1.0),
+                                            child: Image.asset(
+                                              "assets/images/affiliate screen/losing player (1).png",
+                                              width: width / 10,
+                                            ),
                                           ),
-                                          BuildSubHeadingText(
-                                            text: player['username'],
-                                            color: Colors.white,
-                                            fontSize: 12,
+                                          Padding(
+                                            padding:
+                                                const EdgeInsets.only(top: 13),
+                                            child: Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                BuildSubHeadingText(
+                                                  text: "ID:#${player['id']}",
+                                                  color: Colors.white,
+                                                  fontSize: 10,
+                                                ),
+                                                BuildSubHeadingText(
+                                                  text: player['username'],
+                                                  color: Colors.white,
+                                                  fontSize: 12,
+                                                ),
+                                              ],
+                                            ),
                                           ),
                                         ],
                                       ),
-                                    ),
-                                  ],
+                                      Image.asset(
+                                        "assets/images/affiliate screen/aff_transfer button.png",
+                                        width: 40,
+                                      ),
+                                      BuildSubHeadingText(
+                                        text:
+                                            "\$${player['transfer'].toString()}",
+                                        color: Colors.white70,
+                                        fontSize: 9,
+                                      ),
+                                      BuildSubHeadingText(
+                                        text:
+                                            "\$${player['balance'].toString()}",
+                                        color: Colors.green,
+                                        fontSize: 9,
+                                      ),
+                                      BuildSubHeadingText(
+                                        text:
+                                            "\$${player['winnings'].toString()}",
+                                        color: Colors.white70,
+                                        fontSize: 9,
+                                      ),
+                                      BuildSubHeadingText(
+                                        text: "\$${player['rake'].toString()}",
+                                        color: Colors.white70,
+                                        fontSize: 9,
+                                      ),
+                                      BuildSubHeadingText(
+                                        text:
+                                            "\$${player['commission'].toString()}",
+                                        color: Colors.white70,
+                                        fontSize: 9,
+                                      ),
+                                      const SizedBox(width: 15),
+                                    ],
+                                  ),
                                 ),
-                                Image.asset(
-                                  "assets/images/affiliate screen/aff_transfer button.png",
-                                  width: 40,
-                                ),
-                                BuildSubHeadingText(
-                                  text: player['transfer'],
-                                  color: Colors.white70,
-                                  fontSize: 9,
-                                ),
-                                BuildSubHeadingText(
-                                  text: player['balance'],
-                                  color: Colors.green,
-                                  fontSize: 9,
-                                ),
-                                BuildSubHeadingText(
-                                  text: player['winnings'],
-                                  color: Colors.white70,
-                                  fontSize: 9,
-                                ),
-                                BuildSubHeadingText(
-                                  text: player['rake'],
-                                  color: Colors.white70,
-                                  fontSize: 9,
-                                ),
-                                const SizedBox(width: 15),
-                              ],
+                              ),
                             ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  );
-                },
-              ),
+                          ],
+                        );
+                      },
+                    ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget sortButton(String columnName, String label) {
+    return GestureDetector(
+      onTap: () => sortList(columnName),
+      child: Row(
+        children: [
+          BuildSubHeadingText(
+            text: label,
+            color: Colors.white,
+            fontSize: 10,
+          ),
+          if (sortedColumn == columnName)
+            Icon(
+              isAscending
+                  ? Icons.arrow_upward_rounded
+                  : Icons.arrow_downward_rounded,
+              color: Colors.white70,
+              size: 12,
+            ),
+        ],
       ),
     );
   }
